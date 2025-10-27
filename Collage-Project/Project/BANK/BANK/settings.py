9212d049bdce70bf6cbfeb4b089bd85423877449
Apoorv Singh <avsinghrathorft@gmail.com>
2025-10-28 02:06:45 +0530

    add new

--- Collage-Project/Project/BANK/BANK/settings.py
+++ Collage-Project/Project/BANK/BANK/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u)w#rod#z%1aqv_5c!8rt!bkxi8%oc&ju3b$wnn+&i-sz=j9d&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['NetBanking.onrender.com']
 
 
 # Application definition
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+
 ]
 
 ROOT_URLCONF = 'BANK.urls'
